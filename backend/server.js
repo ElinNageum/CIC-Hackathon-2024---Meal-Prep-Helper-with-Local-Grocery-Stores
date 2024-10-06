284b94aa2c36a8323262c4bc6546d5c57f8c646f
marcusg9 <marcusg9@students.cs.ubc.ca>
2024-10-05 18:22:19 -0700

    frontend

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,14 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.options('*', cors()); 
 
 
+
 const getData = async function(){
 const data = scraper.getAllURLS("https://flipp.com/en-ca/vancouver-bc/flyer/6853525-no-frills-weekly-flyer-valid-thursday-october-3-wednesday-october-9?postal_code=V6T1A1");
 const json =  JSON.stringify(data);
 fs.writeFileSync("./data", json, 'utf-8');
 }
 
-
+getData();
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -2,4 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 
+function func(req,res){
+    
+}
+
+
+router.post("/req", func);
+
+
 module.exports = router;
